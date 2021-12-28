17c719b82bf93a568cff34c9f02786cbc00229c4
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2021-12-28 15:40:14 -0500

    Connection string and database

--- HolidayChallenge.Data/IdentityModels.cs
+++ HolidayChallenge.Data/IdentityModels.cs
@@ -34,7 +34,7 @@ namespace HolidayChallenge.Data
         }
 
 
-       
+
         public DbSet<Tree> Trees { get; set; }
         public DbSet<Present> Presents { get; set; }
         public DbSet<Ornament> Ornaments { get; set; }
@@ -50,23 +50,23 @@ namespace HolidayChallenge.Data
                 .Add(new IdentityUserLoginConfiguration())
                 .Add(new IdentityUserRoleConfiguration());
         }
-
-
-        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
-        {
-            public IdentityUserLoginConfiguration()
-            {
-                HasKey(iul => iul.UserId);
-            }
-        }
-
-        public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
-        {
-            public IdentityUserRoleConfiguration()
-            {
-                HasKey(iur => iur.UserId);
-            }
-        }
-
     }
+
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
+
 }
