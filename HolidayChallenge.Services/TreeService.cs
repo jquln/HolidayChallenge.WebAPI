379d1f51f3108797eea0e05b2f3550fcc424a99a
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2021-12-31 18:20:14 -0500

    Added to TreeService

--- HolidayChallenge.Services/TreeService.cs
+++ HolidayChallenge.Services/TreeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HolidayChallenge.Data;
+using HolidayChallenge.Models.TreeModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,45 @@ namespace HolidayChallenge.Services
 {
     public class TreeService
     {
+        private readonly Guid _userId;
+        public TreeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateTree(TreeCreate model)
+        {
+            var entity =
+                new Tree()
+                {
+                    Description = model.Description
+                    
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Trees.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<TreeList> GetTrees()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Trees.AsEnumerable()
+                        .Select(
+                        e =>
+                            new TreeList
+                            {
+                                Id = e.Id,
+                                Description = e.Description
+                            }).ToArray();
+                return query;
+            }
+        }
+
     }
 }
