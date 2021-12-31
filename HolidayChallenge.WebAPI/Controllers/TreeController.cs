afb65c259a878b3b78d931eaae50d819066ad16e
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2021-12-31 18:32:06 -0500

    Added Tree Controller

---
+++ HolidayChallenge.WebAPI/Controllers/TreeController.cs
@@ -0,0 +1,44 @@
+﻿using HolidayChallenge.Models.TreeModels;
+using HolidayChallenge.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HolidayChallenge.WebAPI.Controllers
+{
+    [Authorize]
+    public class TreeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            TreeService treeService = CreateTreeService();
+            var trees = treeService.GetTrees();
+            return Ok(trees);
+        }
+
+        public IHttpActionResult Post(TreeCreate tree)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateTreeService();
+
+            if (!service.CreateTree(tree))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private TreeService CreateTreeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var treeService = new TreeService(userId);
+            return treeService;
+        }
+
+    }
+}
